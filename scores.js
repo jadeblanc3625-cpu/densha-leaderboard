8d3c03ad4bdfd5238783ccb53f1984e4ab0e0eef
jadeblanc3625-cpu <jadeblanc3625@gmail.com>
2026-09-11 20:02:07 +0800

    Add files via upload

--- scores.js
+++ scores.js
@@ -42,6 +42,7 @@ const PLAYERS = [
   { name: "阿尔托莉雅", score: 1583928, platform: "Steam", video: "", date: "2026-09-11" },
   { name: "我变强了！", score: 357829, platform: "Steam", video: "", date: "2026-09-11" },
   { name: "安念研", score: 4718984, platform: "Steam", video: "", date: "2026-09-11" },
+  { name: "子扬", score: 7519129, platform: "Steam", video: "", date: "2026-09-11" },
 ];
 
 // 积分规则：每届最终排名前十名依次得 10、9、…、1 分；第 11 名及以后 0 分。
